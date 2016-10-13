626eb000fa2a18aa35f86290ceacc3da3981f77b
Julien Harbulot <julien.harbulot@epfl.ch>
2016-10-13 17:57:21 +0200

    Create core architecture
    Update gradle version to 2.2.1
    
    Silence android lint bug warning

---
+++ IcyNote/app/src/main/java/icynote/core/OrderType.java
@@ -0,0 +1,12 @@
+package icynote.core;
+
+/**
+ * Enumerates whether the order is ascending or descending.
+ *
+ * @author Julien Harbulot
+ * @version 1.0
+ * @see icynote.core.IcyNoteCore#getNotes(OrderBy, OrderType)
+ */
+public enum OrderType {
+    ASC, DSC
+}
---
+++ IcyNote/app/src/test/java/icynote/core/NoteTests.java
@@ -0,0 +1,135 @@
+package icynote.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Unit tests for a {@code Note}; may be subclassed and
+ * specialized to test specific implementations.
+ */
+
+@SuppressWarnings("CanBeFinal")
+public abstract class NoteTests {
+
+    private static final int YEAR = 2000;
+    // keep those attributes protected, some interactor might have a validation rule
+    // that needs a different value than those proposed here
+    protected Note toTest = null;
+    protected String originalTitle = "NoteTests.title";
+    protected String originalContent = "NoteTests.content";
+    protected GregorianCalendar originalCreation = new GregorianCalendar(YEAR, 1, 1);
+    protected GregorianCalendar originalUpdate = new GregorianCalendar(YEAR + 1, 1, 1);
+
+    protected Note addInteractor(Note data) {
+        return data;
+    }
+
+    protected abstract Note makeData();
+
+    //---------------------------------------------------------------------------------
+
+    @Before
+    public void reset() {
+        toTest = makeData();
+        toTest.setTitle(originalTitle);
+        toTest.setContent(originalContent);
+        toTest.setCreation(originalCreation);
+        toTest.setLastUpdate(originalUpdate);
+        toTest = addInteractor(toTest);
+    }
+
+    //---------------------------------------------------------------------------------
+
+    @Test
+    public void setId() {
+        int id = 100;
+        Response r = toTest.setId(id);
+
+        assertNotNull("response is not null", r);
+        assertTrue("response is positive", r.isPositive());
+        assertEquals("check new value", toTest.getId(), id);
+    }
+
+    @Test
+    public void setCreation() {
+        GregorianCalendar d = new GregorianCalendar();
+        Response r = toTest.setCreation(d);
+
+        assertNotNull("response is not null", r);
+        assertTrue("response is positive", r.isPositive());
+        assertEquals("check new value", toTest.getCreation(), d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setCreationNull() {
+        toTest.setCreation(null);
+    }
+
+
+    @Test
+    public void setLastUpdate() {
+        GregorianCalendar d = new GregorianCalendar();
+        Response r = toTest.setLastUpdate(d);
+
+        assertNotNull("response is not null", r);
+        assertTrue("response is positive", r.isPositive());
+        assertEquals("check new value", toTest.getLastUpdate(), d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setLastUpdateNull() {
+        toTest.setLastUpdate(null);
+    }
+
+
+    @Test
+    public void setTitle() {
+        String newTitle = "another title";
+        Response r = toTest.setTitle(newTitle);
+
+        assertTrue("check setTitle response", r.isPositive());
+        assertEquals("check new title value", newTitle, toTest.getTitle());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setTitleNull() {
+        toTest.setTitle(null);
+    }
+
+
+    @Test
+    public void setContent() {
+        String newContent = "a better content";
+        Response r = toTest.setContent(newContent);
+
+        assertTrue(r.isPositive());
+        assertEquals(newContent, toTest.getContent());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setContentNull() {
+        toTest.setContent(null);
+    }
+
+    @Test
+    public void getCreationDefensiveCopy() {
+        GregorianCalendar d = toTest.getCreation();
+        GregorianCalendar e = toTest.getCreation();
+        assertNotSame(d, e);
+    }
+
+    @Test
+    public void getModificationDefensiveCopy() {
+        GregorianCalendar d = toTest.getLastUpdate();
+        GregorianCalendar e = toTest.getLastUpdate();
+        assertNotSame(d, e);
+    }
+}
