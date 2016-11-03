9eb87671bfdfe61f5d0cf7a8ba90bf318e44683f
roblan11 <roblan11@hotmail.ch>
2016-11-03 20:30:37 +0100

    delete outdated classes, general cleanup

--- IcyNote/app/src/main/java/icynote/ui/Settings.java
+++ IcyNote/app/src/main/java/icynote/ui/Settings.java
@@ -26,8 +26,6 @@ public class Settings extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Style.ColorSetting curr = Style.getStyle();
-        container.setBackgroundColor(curr.getBackgroundColor());
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         setSpinnerStyles(view);
@@ -47,18 +45,7 @@ public class Settings extends Fragment {
         spinnerStyles.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
 
     }
