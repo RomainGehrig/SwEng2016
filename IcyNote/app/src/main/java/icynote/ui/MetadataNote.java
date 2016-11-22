03bc0b67b05b383ce22dad0b010b9a942cc22d87
Julien Harbulot <julien.harbulot@epfl.ch>
2016-11-22 12:23:20 +0100

    Remove themes

--- IcyNote/app/src/main/java/icynote/ui/MetadataNote.java
+++ IcyNote/app/src/main/java/icynote/ui/MetadataNote.java
@@ -1,9 +1,7 @@
 package icynote.ui;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,20 +22,20 @@ public class MetadataNote extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // create ContextThemeWrapper from the original Activity Context with the custom theme
-        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), Theme.getTheme().toInt());
+        //final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), Theme.getTheme().toInt());
 
         // clone the inflater using the ContextThemeWrapper
-        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+        LayoutInflater localInflater = inflater; //inflater.cloneInContext(contextThemeWrapper);
 
         // inflate the layout using the cloned inflater, not default inflater
         View v = localInflater.inflate(R.layout.fragment_metadata_note, container, false);
 
         TextView checkBox1 = (TextView)v.findViewById(R.id.checkBox1);
-        checkBox1.setTextColor(Theme.getTheme().getTextColor());
+        //checkBox1.setTextColor(Theme.getTheme().getTextColor());
         TextView checkBox2 = (TextView)v.findViewById(R.id.checkBox2);
-        checkBox2.setTextColor(Theme.getTheme().getTextColor());
+        //checkBox2.setTextColor(Theme.getTheme().getTextColor());
         TextView checkBox3 = (TextView)v.findViewById(R.id.checkBox3);
-        checkBox3.setTextColor(Theme.getTheme().getTextColor());
+        //checkBox3.setTextColor(Theme.getTheme().getTextColor());
 
         return v;
     }
