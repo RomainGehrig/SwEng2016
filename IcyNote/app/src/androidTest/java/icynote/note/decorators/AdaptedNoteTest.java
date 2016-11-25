a7893a09936f3114707f41bd8c6b7689cef20835
KL <kim.phanhoang@epfl.ch>
2016-11-25 22:40:49 +0100

    Continue test for AdaptedNote

--- IcyNote/app/src/androidTest/java/icynote/note/decorators/AdaptedNoteTest.java
+++ IcyNote/app/src/androidTest/java/icynote/note/decorators/AdaptedNoteTest.java
@@ -1,42 +1,62 @@
-/*
 package icynote.note.decorators;
 
 import android.text.SpannableString;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import icynote.note.Note;
 import icynote.note.impl.NoteData2;
 import util.Adapter;
 
-import static org.junit.Assert.*;
-
-*/
-/**
- * Created by kl on 25.11.2016.
- *//*
+import static org.junit.Assert.assertEquals;
 
 public class AdaptedNoteTest {
 
-    private Adapter<SpannableString, String> adapter = new Adapter<SpannableString, String>() {
+    private static Adapter<String, SpannableString> adapter = new Adapter<String, SpannableString>() {
         @Override
-        public String from(SpannableString b) {
-            return ""+b;
+        public SpannableString from(String b) {
+            return new SpannableString(b);
         }
 
         @Override
-        public SpannableString to(String a) {
-            return new SpannableString(a);
+        public String to(SpannableString a) {
+            return ""+a;
         }
     };
 
-    private Note<SpannableString> note = new NoteData2();
+    private static AdaptedNote<String, SpannableString> adaptedNote;
+    private static Note<SpannableString> note;
 
-    private AdaptedNote<SpannableString, String> adaptedNote = new AdaptedNote<>(note, adapter);
 
-    @Test
-    public void getAdaptedNote() throws Exception {
-
+    @BeforeClass
+    public static void setUp() {
+        note = new NoteData2();
+        note.setId(1);
+        note.setTitle(new SpannableString("title"));
+        note.setContent(new SpannableString("content"));
+        adaptedNote = new AdaptedNote<>(note, adapter);
     }
 
-}*/
+
+    @Test
+    public void getAdaptedNoteTest() {
+        assertEquals(note, adaptedNote.getAdaptedNote());
+    }
+
+    @Test
+    public void getIdTest() {
+        assertEquals(note.getId(),adaptedNote.getId());
+    }
+
+    @Test
+    public void getTitleTest() {
+        assertEquals(adapter.to(note.getTitle()),adaptedNote.getTitle()); // return String
+    }
+
+    @Test
+    public void getContentTest() {
+        assertEquals(adapter.to(note.getContent()),adaptedNote.getContent()); // return String
+    }
+
+}
