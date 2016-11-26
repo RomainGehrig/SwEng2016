0ab13626d4f43a5adc0f99f14180c0c80c94a9e2
roblan11 <roblan11@hotmail.ch>
2016-11-26 16:25:17 +0100

    Reformat Preferences to be a Fragment, import the list of plugins correctly into the preferences (closes issue #101)

--- IcyNote/app/src/main/java/icynote/ui/fragments/Preferences.java
+++ IcyNote/app/src/main/java/icynote/ui/fragments/Preferences.java
@@ -1,12 +1,18 @@
-package icynote.ui;
+package icynote.ui.fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.text.SpannableString;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,34 +20,55 @@ import java.util.List;
 
 import icynote.login.LoginManager;
 import icynote.login.LoginManagerFactory;
+import icynote.note.Note;
 import icynote.noteproviders.OrderBy;
 import icynote.noteproviders.OrderType;
+import icynote.plugins.Plugin;
+import icynote.plugins.PluginsProvider;
+import icynote.ui.CurrentPreferences;
+import icynote.ui.R;
+import icynote.ui.contracts.NoteOptionsPresenter;
 import icynote.ui.loginactivities.GoogleLinkCredentials;
 import icynote.ui.loginactivities.GoogleUnLinkCredentials;
 
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceFragment {
 
     static private final int RC_LINK_GOOGLE = 1505;
     static private final int RC_UNLINK_GOOGLE = 1505;
 
     private ListPreference orderByPref;
     private ListPreference orderTypePref;
-
     private PreferenceScreen pluginPref;
-
     private Preference accountPref;
-
     private Preference deleteAccPref;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public Preferences() {
+        // Required empty public constructor
+    }
 
-        // Load the preferences from an XML resource
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(Color.BLACK);
+
         addPreferencesFromResource(R.xml.preferences);
 
+        setOrderPrefs();
+        updateAccountButtons();
+        setPlugins();
 
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(getActivity(), "Actions not implemented, yet.", Toast.LENGTH_LONG).show();
+    }
+
+    private void setOrderPrefs() {
         orderByPref = (ListPreference) findPreference("select_sort_by");
         orderByPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
@@ -74,49 +101,9 @@ public class Preferences extends PreferenceActivity {
                 return true;
             }
         });
-
-        updateButton();
-
-        deleteAccPref = findPreference("account_delete");
-        deleteAccPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                deleteAccount();
-                return true;
-            }
-        });
-
-        List<String> plugins = new ArrayList<>();
-        plugins.add("plugin 1");
-        plugins.add("plugin 2");
-        plugins.add("plugin 3");
-
-        pluginPref = (PreferenceScreen) findPreference("plugin_settings");
-        for(final String plugin: plugins){
-            final SwitchPreference curr = new SwitchPreference(this);
-            curr.setTitle(plugin);
-            curr.setKey(plugin);
-            curr.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if(!(Boolean) newValue){
-                        curr.setSummary("disabled");
-                    } else {
-                        curr.setSummary("");
-                    }
-                    return true;
-                }
-            });
-            pluginPref.addPreference(curr);
-        }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "Actions not implemented, yet.", Toast.LENGTH_LONG).show();
-    }
-
-    public void updateButton() {
+    private void updateAccountButtons() {
         accountPref = findPreference("account_action");
         LoginManager loginManager = LoginManagerFactory.getInstance();
         if(loginManager.userCanLoginWithEmail()){
@@ -128,28 +115,36 @@ public class Preferences extends PreferenceActivity {
         } else {
             setAccountPref("Generate password", 2);
         }
-    }
 
-    public void setAccountPref(String title, final int mode) {
-        accountPref.setTitle(title);
-        accountPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        deleteAccPref = findPreference("account_delete");
+        deleteAccPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-                execAccountAction(mode);
-                updateButton();
+                deleteAccount();
                 return true;
             }
         });
     }
 
-    public void execAccountAction(int mode){
+    private void setAccountPref(String title, final int mode) {
+        accountPref.setTitle(title);
+        accountPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                execAccountAction(mode);
+                updateAccountButtons();
+                return true;
+            }
+        });
+    }
+
+    private void execAccountAction(int mode){
         switch (mode){
             case 0: {
-                Intent intent = new Intent(this, GoogleLinkCredentials.class);
+                Intent intent = new Intent(getActivity(), GoogleLinkCredentials.class);
                 startActivityForResult(intent, RC_LINK_GOOGLE);
                 break;
             }
             case 1: {
-                Intent intent = new Intent(this, GoogleUnLinkCredentials.class);
+                Intent intent = new Intent(getActivity(), GoogleUnLinkCredentials.class);
                 startActivityForResult(intent, RC_UNLINK_GOOGLE);
                 break;
             }
@@ -162,8 +157,36 @@ public class Preferences extends PreferenceActivity {
     }
 
 
-    public void deleteAccount() {
+    private void deleteAccount() {
         // TODO delete local account
+    }
+
+    private void setPlugins() {
+        pluginPref = (PreferenceScreen) findPreference("plugin_settings");
+        Iterable<Plugin> allPlugins = (new PluginsProvider()).getPlugins();
+        List<String> allPluginNames = new ArrayList<>();
+        for(Plugin plugin: allPlugins) {
+            Iterable<String> currentPlugin = plugin.getNames();
+            for(String pluginName: currentPlugin) {
+                final SwitchPreference thisPlugin = new SwitchPreference(getActivity());
+                thisPlugin.setTitle(pluginName);
+                thisPlugin.setKey(pluginName);
+                thisPlugin.setDefaultValue(true);
+                thisPlugin.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        if(!(Boolean) newValue){
+                            thisPlugin.setSummary("disabled");
+                            // TODO disable / enable plugin
+                        } else {
+                            thisPlugin.setSummary("");
+                        }
+                        return true;
+                    }
+                });
+                pluginPref.addPreference(thisPlugin);
+            }
+        }
     }
 
 }
