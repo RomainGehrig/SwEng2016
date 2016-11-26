f2f1c913bd419e24b098ec3e25f466e6de9ee4b8
KL <kim.phanhoang@epfl.ch>
2016-11-26 19:29:54 +0100

    Adds tests for AdaptedNote

--- IcyNote/app/src/androidTest/java/icynote/note/decorators/AdaptedNoteTest.java
+++ IcyNote/app/src/androidTest/java/icynote/note/decorators/AdaptedNoteTest.java
@@ -5,6 +5,8 @@ import android.text.SpannableString;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.GregorianCalendar;
+
 import icynote.note.Note;
 import icynote.note.impl.NoteData2;
 import util.Adapter;
@@ -25,17 +27,13 @@ public class AdaptedNoteTest {
         }
     };
 
-    private static AdaptedNote<String, SpannableString> adaptedNote;
-    private static Note<SpannableString> note;
+    private Note<SpannableString> note = new NoteData2();
+    private AdaptedNote<String, SpannableString> adaptedNote = new AdaptedNote<>(note, adapter);
 
 
     @BeforeClass
     public static void setUp() {
-        note = new NoteData2();
-        note.setId(1);
-        note.setTitle(new SpannableString("title"));
-        note.setContent(new SpannableString("content"));
-        adaptedNote = new AdaptedNote<>(note, adapter);
+
     }
 
 
@@ -46,17 +44,67 @@ public class AdaptedNoteTest {
 
     @Test
     public void getIdTest() {
-        assertEquals(note.getId(),adaptedNote.getId());
+        int id = 1;
+        note.setId(id);
+        assertEquals(id, adaptedNote.getId());
     }
 
     @Test
     public void getTitleTest() {
-        assertEquals(adapter.to(note.getTitle()),adaptedNote.getTitle()); // return String
+        String title = "title";
+        note.setTitle(new SpannableString(title));
+        assertEquals(title, adaptedNote.getTitle()); // return String
     }
 
     @Test
     public void getContentTest() {
-        assertEquals(adapter.to(note.getContent()),adaptedNote.getContent()); // return String
+        String content = "content";
+        note.setContent(new SpannableString(content));
+        assertEquals(content, adaptedNote.getContent()); // return String
+    }
+
+    @Test
+    public void getCreationTest() {
+        GregorianCalendar date = new GregorianCalendar();
+        note.setCreation(date);
+        assertEquals(date, adaptedNote.getCreation());
+    }
+
+    @Test
+    public void getLastUpdateTest() {
+        GregorianCalendar newDate = new GregorianCalendar();
+        note.setLastUpdate(newDate);
+        assertEquals(newDate, adaptedNote.getLastUpdate());
+    }
+
+    @Test
+    public void setIdTest() {
+        int newId = 2;
+        assertEquals(note.setId(newId).isPositive(),adaptedNote.setId(newId).isPositive());
+    }
+
+    @Test
+    public void setTitleTest() {
+        String newTitle = "newTitle";
+        assertEquals(note.setTitle(new SpannableString(newTitle)).isPositive(),adaptedNote.setTitle(newTitle).isPositive());
+    }
+
+    @Test
+    public void setContentTest() {
+        String newContent = "newContent";
+        assertEquals(note.setTitle(new SpannableString(newContent)).isPositive(),adaptedNote.setTitle(newContent).isPositive());
+    }
+
+    @Test
+    public void setCreationTest() {
+        GregorianCalendar newCreation = new GregorianCalendar();
+        assertEquals(note.setCreation(newCreation).isPositive(),adaptedNote.setCreation(newCreation).isPositive());
+    }
+
+    @Test
+    public void setLastUpdateTest() {
+        GregorianCalendar newLastUpdate = new GregorianCalendar();
+        assertEquals(note.setLastUpdate(newLastUpdate).isPositive(),adaptedNote.setLastUpdate(newLastUpdate).isPositive());
     }
 
 }
