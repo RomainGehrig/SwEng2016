eb896fdbee2c17cfa5533131adfd83fe3bc7e7b2
KL <kim.phanhoang@epfl.ch>
2016-11-27 12:55:34 +0100

    Create tests for AdaptedProvider

---
+++ IcyNote/app/src/androidTest/java/icynote/noteproviders/decorators/AdaptedProviderTest.java
@@ -0,0 +1,50 @@
+package icynote.noteproviders.decorators;
+
+import android.text.SpannableString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import icynote.note.Note;
+import icynote.note.impl.NoteData;
+import icynote.noteproviders.NoteProvider;
+import icynote.noteproviders.persistent.ListNoteProvider;
+import icynote.noteproviders.templates.NoteProviderTests;
+import util.Adapter;
+
+
+// FIXME
+/*
+public class AdaptedProviderTest extends NoteProviderTests {
+
+    Adapter<String, SpannableString> adapter = new Adapter<String, SpannableString>() {
+
+        @Override
+        public SpannableString from(String b) {
+            return new SpannableString(b);
+        }
+
+        @Override
+        public String to(SpannableString a) {
+            return ""+a;
+        }
+    };
+
+    @Override
+    protected NoteProvider makeNew() {
+        return new AdaptedProvider(new ListNoteProvider(), adapter);
+    }
+
+    @Override
+    protected NoteProvider makeNewWith(Note<String> n1, Note<String> n2, Note<String> n3) {
+        List<Note<String>> list = new ArrayList<>();
+        list.add(n1); list.add(n2); list.add(n3);
+        ListNoteProvider listNoteProvider = new ListNoteProvider(list);
+        return new AdaptedProvider(listNoteProvider, adapter);
+    }
+
+    @Override
+    protected Note<String> makeNewNote() {
+        return new NoteData();
+    }
+}*/
