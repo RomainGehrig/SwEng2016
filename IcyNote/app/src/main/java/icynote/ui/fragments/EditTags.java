5ac8dfd06badaf5c5d702e00c7a84e027cab9e6f
KL <kim.phanhoang@epfl.ch>
2016-12-05 21:31:20 +0100

    Fix lint issue for EditTags (unused method, overriding method only calling super method)

--- IcyNote/app/src/main/java/icynote/ui/fragments/EditTags.java
+++ IcyNote/app/src/main/java/icynote/ui/fragments/EditTags.java
@@ -1,6 +1,5 @@
 package icynote.ui.fragments;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,19 +14,10 @@ public class EditTags extends FragmentWithState {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // inflate the layout using the cloned inflater, not default inflater
         return inflater.inflate(R.layout.fragment_edit_tags, container, false);
     }
 
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
-    }
 }
