58e8d1701e67de1b7ca2834f737f426f189a6f74
KL <kim.phanhoang@epfl.ch>
2016-12-07 18:07:45 +0100

    Resize photos depending on screen size

--- IcyNote/app/src/main/java/icynote/plugins/ImageFormatter.java
+++ IcyNote/app/src/main/java/icynote/plugins/ImageFormatter.java
@@ -1,12 +1,15 @@
 package icynote.plugins;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -35,7 +38,9 @@ import icynote.note.Note;
 import icynote.note.Response;
 import icynote.note.decorators.NoteDecoratorFactory;
 import icynote.note.decorators.NoteDecoratorTemplate;
+import icynote.ui.MainActivity;
 import icynote.ui.R;
+import icynote.ui.fragments.EditNote;
 import util.Callback;
 
 import static android.R.attr.minHeight;
@@ -343,7 +348,7 @@ class ImageFormatter implements FormatterPlugin {
     }
     private static ImageSpanWithId getImageSpan(Resources r, Bitmap image, String name) {
         Drawable d = new BitmapDrawable(r, image);
-        // TODO image peut dépasser un peu sur le côté
+
         d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
         return new ImageSpanWithId(name, d, ImageSpan.ALIGN_BASELINE);
     }
@@ -351,7 +356,8 @@ class ImageFormatter implements FormatterPlugin {
         Uri uri = Uri.parse(name);
         //bitmap = MediaStore.Images.Media.getBitmap(
         //        appState.getActivity().getContentResolver(), uri);
-        return decodeSampledBitmapFromResource(appState, uri, minWidth, minHeight);
+
+        return decodeSampledBitmapFromResource(appState, uri, MainActivity.screenWidth/3, MainActivity.screenHeight/3);
     }
     private static Bitmap decodeSampledBitmapFromResource(PluginData state,
                                                           Uri uri, int reqWidth, int reqHeight) {
@@ -391,11 +397,61 @@ class ImageFormatter implements FormatterPlugin {
 
         return BitmapFactory.decodeStream(is, null, options);
     }
+
+
+    // ROTATION
+
     /**
-     * Sample Size : load less https://developer.android.com/training/displaying-bitmaps/load-bitmap.html
-     **/
-    private static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+     * This method is responsible for solving the rotation issue if exist. Also scale the images to
+     * 1024x1024 resolution
+     *
+     * @param context       The current context
+     * @param selectedImage The Image URI
+     * @return Bitmap image results
+     * @throws IOException
+     */
+    public static Bitmap handleSamplingAndRotationBitmap(Context context, Uri selectedImage)
+            throws IOException {
+        int MAX_HEIGHT = 1024;
+        int MAX_WIDTH = 1024;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream imageStream = context.getContentResolver().openInputStream(selectedImage);
+        BitmapFactory.decodeStream(imageStream, null, options);
+        imageStream.close();
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        imageStream = context.getContentResolver().openInputStream(selectedImage);
+        Bitmap img = BitmapFactory.decodeStream(imageStream, null, options);
+
+
+        img = rotateImageIfRequired(context, img, selectedImage);
+        return img;
+    }
+
+
+    /**
+     * Calculate an inSampleSize for use in a {@link BitmapFactory.Options} object when decoding
+     * bitmaps using the decode* methods from {@link BitmapFactory}. This implementation calculates
+     * the closest inSampleSize that will result in the final decoded bitmap having a width and
+     * height equal to or larger than the requested width and height. This implementation does not
+     * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
+     * results in a larger bitmap which isn't as useful for caching purposes.
+     *
+     * @param options   An options object with out* params already populated (run through a decode*
+     *                  method with inJustDecodeBounds==true
+     * @param reqWidth  The requested width of the resulting bitmap
+     * @param reqHeight The requested height of the resulting bitmap
+     * @return The value to be used for inSampleSize
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
@@ -403,17 +459,66 @@ class ImageFormatter implements FormatterPlugin {
 
         if (height > reqHeight || width > reqWidth) {
 
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
 
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
+            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+            // with both dimensions larger than or equal to the requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+
+            final float totalPixels = width * height;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++;
             }
         }
-
         return inSampleSize;
+    }
+
+
+
+    /**
+     * Rotate an image if required.
+     *
+     * @param img           The image bitmap
+     * @param selectedImage Image URI
+     * @return The resulted Bitmap after manipulation
+     */
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+
+        // TODO ne match pas les paths !
+        ExifInterface ei = new ExifInterface(selectedImage.getPath());
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
     }
 }
