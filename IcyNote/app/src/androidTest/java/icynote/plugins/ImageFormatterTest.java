b1c3a3849713d013367e67fe1ae74beb44a8c3c9
Julien Harbulot <julien.harbulot@epfl.ch>
2016-12-14 22:50:01 +0100

    Remove obsolete ImageFormatterTest

--- IcyNote/app/src/androidTest/java/icynote/plugins/ImageFormatterTest.java
+++ IcyNote/app/src/androidTest/java/icynote/plugins/ImageFormatterTest.java
@@ -1,9 +1,8 @@
 package icynote.plugins;
 
-import android.graphics.BitmapFactory;
-
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ImageFormatterTest {
 
@@ -13,30 +12,4 @@ public class ImageFormatterTest {
     public void getNameTest() {
         assertEquals(formatter.getName(), "Image insertion Plugin");
     }
-
-    @Test
-    public void calculateInSampleSizeTest() {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.outHeight = 1000;
-        options.outWidth = 1500;
-
-        ImageFormatterForTest imageFormatter = new ImageFormatterForTest(0, 0);
-        int res = imageFormatter.calculateInSampleSize(options, 100, 100);
-        assertEquals(8, res);
-        int res2 = imageFormatter.calculateInSampleSize(options, 20, 20);
-        assertEquals(32, res2);
-    }
-
 }
-
-class ImageFormatterForTest extends ImageFormatter {
-        public ImageFormatterForTest(int requestCodeCamera, int requestCodeGallery) {
-            super(requestCodeCamera, requestCodeGallery);
-        }
-
-        protected static int calculateInSampleSize(BitmapFactory.Options options,
-                                                   int reqWidth,
-                                                   int reqHeight) {
-            return ImageFormatter.calculateInSampleSize(options, reqWidth, reqHeight);
-        }
-    }
