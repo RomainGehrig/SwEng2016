5ca63b0e866ecb219b792a737ef3d49135e14d2b
KL <kim.phanhoang@epfl.ch>
2016-12-14 11:28:27 +0100

    Finish test for PictureEditor

--- IcyNote/app/src/androidTest/java/icynote/plugins/PictureEditorTest.java
+++ IcyNote/app/src/androidTest/java/icynote/plugins/PictureEditorTest.java
@@ -2,6 +2,7 @@ package icynote.plugins;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Environment;
@@ -13,8 +14,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import icynote.ui.BlankActivity;
 import icynote.ui.R;
@@ -24,16 +28,18 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by kl on 13.12.2016.
+ * @author Kim Lan Phan Hoang
  */
 public class PictureEditorTest {
 
     private BlankActivity mActivity;
     private Uri uri;
     private String absPath;
-    private Bitmap bitmap;
 
     @Rule
     public final ActivityTestRule<BlankActivity> main = new ActivityTestRule<>(BlankActivity.class);
@@ -41,25 +47,28 @@ public class PictureEditorTest {
     @Before
     public void setUp() throws Exception {
         mActivity = main.getActivity();
+        // add image into the content resolver
         addImageInContentResolver();
+        // start picture editor
+        startPictureEditor();
+    }
 
+    private void startPictureEditor() {
         // start PictureEditor with uri and path in extra
         Intent i = new Intent(mActivity, PictureEditor.class);
         i.putExtra("uri", uri.toString());
         i.putExtra("absolutePath", absPath);
         mActivity.startActivity(i);
-
     }
 
     private void addImageInContentResolver() {
 
         // Create a Bitmap
-        bitmap = Bitmap.createBitmap(80, 100,Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(80, 100,Bitmap.Config.ARGB_8888);
         TextView tv = new TextView(mActivity);
         Canvas c = new Canvas(bitmap);
         tv.layout(0, 0, 80, 100);
         tv.draw(c);
-
 
         // save it into file
         File storageDir = mActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
@@ -82,22 +91,56 @@ public class PictureEditorTest {
         uri = Uri.fromFile(file);
     }
 
-
-    private void startingView() {
+    @Test
+    public void startingView() {
         onView(withId(R.id.preview_image)).check(matches(isDisplayed()));
         onView(withId(R.id.rotate_left)).check(matches(isDisplayed()));
         onView(withId(R.id.rotate_right)).check(matches(isDisplayed()));
         onView(withId(R.id.validate)).check(matches(isDisplayed()));
     }
 
+    private Bitmap getBitmapFromFile() {
+        InputStream inputStream;
+        Bitmap b = null;
+        try {
+            inputStream = new FileInputStream(absPath);
+            b = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
+
+
     @Test
-    public void rotateTest() {
+    public void rotateLeftTest() {
+        Bitmap oldB = getBitmapFromFile();
+        onView(withId(R.id.rotate_left)).perform(click());
+        onView(withId(R.id.validate)).perform(click());
+        Bitmap newB = getBitmapFromFile();
+        assertEquals(oldB.getHeight(), newB.getWidth());
+        assertFalse(oldB.sameAs(newB));
+    }
+
+    @Test
+    public void rotateLeftAndRightTest() {
+        Bitmap oldB = getBitmapFromFile();
         onView(withId(R.id.rotate_left)).perform(click());
         onView(withId(R.id.rotate_right)).perform(click());
+        onView(withId(R.id.validate)).perform(click());
+        Bitmap newB = getBitmapFromFile();
+        assertTrue(oldB.sameAs(newB));
+    }
 
-        // the view is still there
-        startingView();
-
-        // TODO check rotate is correct (check size)
+    @Test
+    public void rotateLeftTwiceTest() {
+        Bitmap oldB = getBitmapFromFile();
+        onView(withId(R.id.rotate_left)).perform(click(), click());
+        onView(withId(R.id.validate)).perform(click());
+        Bitmap newB = getBitmapFromFile();
+        assertEquals(oldB.getWidth(), newB.getWidth());
     }
 }
